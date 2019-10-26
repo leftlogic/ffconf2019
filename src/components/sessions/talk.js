18f25c54976c25e2260b6ae41ff67efcdfe484e9
Giulia Alfonsi <electric.g@gmail.com>
2019-10-26 18:38:26 +0200

    Sneaking in a css fix to the speaker twitter tag

--- src/components/sessions/talk.js
+++ src/components/sessions/talk.js
@@ -57,7 +57,7 @@ const SpeakerName = ({ name, twitter }) => {
       <div className="speaker__name tag">{name}</div>
       <a
         href={`https://twitter.com/${twitter}`}
-        className="speaker__name tag speaker__twitter"
+        className="speaker__twitter tag"
         target="_blank"
         rel="noopener"
       >
