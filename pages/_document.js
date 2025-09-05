4223320e2eb303480afa405fe62aa17bfee04592
Remy Sharp <remy@remysharp.com>
2025-09-05 16:37:34 +0100

    Add event status banner to _document.js
    Added an event status banner with styling to the document.

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,32 @@ export default class MyDocument extends Document {
           <Meta />
         </Head>
         <body>
+          <div class="event_status" role="region" aria-label="Event status">
+            <span>This event has been and gone, <a href="https://ffconf.org/latest">click here to see the latest
+                FFConf</a></span>
+            <style>
+              .event_status {
+                position: sticky;
+                top: 0;
+                width: 100%;
+                left: 0;
+                background: white;
+                padding: 12px 16px;
+                text-align: center;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
+      
+                * {
+                  color: black;
+                }
+      
+                a {
+                  color: inherit;
+                  text-decoration: underline;
+                }
+              }
+            </style>
+          </div>
+        
           <Main />
           <NextScript />
           <script src="/static/js/script.js" />
